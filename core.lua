5710dbf3b680c6146da7f6666c926c9b90c75372
d87 <d87d87@gmail.com>
2019-10-11 00:32:00 +0700

    CL output function

--- core.lua
+++ core.lua
@@ -799,3 +799,29 @@ function lib:UnregisterFrame(frame)
     end
 end
 lib.Unregister = lib.UnregisterFrame
+
+
+function lib:ToggleDebug()
+    if not lib.debug then
+        lib.debug = CreateFrame("Frame")
+        lib.debug:SetScript("OnEvent",function( self, event )
+            local timestamp, eventType, hideCaster,
+            srcGUID, srcName, srcFlags, srcFlags2,
+            dstGUID, dstName, dstFlags, dstFlags2,
+            spellID, spellName, spellSchool, auraType, amount = CombatLogGetCurrentEventInfo()
+            local isSrcPlayer = (bit_band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) == COMBATLOG_OBJECT_AFFILIATION_MINE)
+            if isSrcPlayer then
+                print (GetTime(), "ID:", spellID, spellName, eventType, srcFlags, srcGUID,"|cff00ff00==>|r", dstGUID, dstFlags, auraType, amount)
+            end
+        end)
+    end
+    if not lib.debug.enabled then
+        lib.debug:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        lib.debug.enabled = true
+        print("[LCD] Enabled combat log event display")
+    else
+        lib.debug:UnregisterAllEvents()
+        lib.debug.enabled = false
+        print("[LCD] Disabled combat log event display")
+    end
+end
